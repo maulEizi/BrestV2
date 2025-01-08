f51674f2eb9c9a326aeb78c9e6366125c95230a5
maulEizi <Pauldubled@outlook.fr>
2025-01-08 16:00:00 +0100

    Update ban.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,25 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Répond avec Pong!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Bannir un membre du serveur')
+    .addUserOption(option => option.setName('user').setDescription('Membre à bannir').setRequired(true))
+    .addStringOption(option => option.setName('reason').setDescription('Raison du bannissement').setRequired(false)),
+  
+  async execute(interaction) {
+    const member = interaction.options.getMember('user');
+    const reason = interaction.options.getString('reason') || 'Aucune raison spécifiée';
+
+    if (!interaction.member.permissions.has('BAN_MEMBERS')) {
+      return interaction.reply('Tu n\'as pas la permission de bannir des membres.');
+    }
+
+    if (!member.bannable) {
+      return interaction.reply('Je ne peux pas bannir ce membre.');
+    }
+
+    await member.ban({ reason: reason });
+    return interaction.reply(`${member.user.tag} a été banni pour : ${reason}`);
+  }
 };
